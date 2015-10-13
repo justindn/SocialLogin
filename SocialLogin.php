fd74f238a1a501ee99fa7274afb13dbb78b77dd7
justindn <justindn@mail.ru>
2015-10-13 14:47:38 +0200

    Login withGoogle Plus added

--- SocialLogin.php
+++ SocialLogin.php
@@ -1,10 +1,10 @@
 <?php
 
-//namespace app\models;
+/* namespace app\models;
 
-//use Yii;
-//use yii\base\Model;
-//use yii\web\Session;
+use Yii;
+use yii\base\Model;
+use yii\web\Session; */
 
 
 class SocialLogin //extends Model
@@ -15,7 +15,7 @@ class SocialLogin //extends Model
     
     public $access_token_url;
     
-	public $user_data_url;
+    public $user_data_url;
 	
     public $app_id;
     public $app_secret_code;
@@ -32,6 +32,7 @@ class SocialLogin //extends Model
 		
 		$network_name = strtolower($network_name);
 		$this->network_name = $network_name;
+		//$this->redirect_uri = $_SERVER['HTTP_HOST'];
 		
 		switch ($network_name){
 			case 'facebook':
@@ -46,105 +47,144 @@ class SocialLogin //extends Model
 				$this->access_token_url = 'https://www.linkedin.com/uas/oauth2/accessToken';
 				$this->user_data_url = 'https://api.linkedin.com/v1/people/~';
 				$this->response_type = 'code';
-				$this->scope = 'r_basicprofile';
+				$this->scope = 'r_basicprofile r_emailaddress';
 				break;
+			case 'googleplus':
+				$this->oauth_url = 'https://accounts.google.com/o/oauth2/auth';
+				$this->access_token_url = 'https://accounts.google.com/o/oauth2/token';
+				$this->user_data_url = 'https://www.googleapis.com/oauth2/v1/userinfo';
+				$this->response_type = 'code';
+				$this->scope = 'profile email';
+
 		}
 	}
 	
     public function getAuthCode(){
-			$app_id = 'app_id';
-			switch ($this->network_name){
-				case 'facebook':
-					$app_id = 'app_id';
-					
-					break;
-				case 'linkedin':
-					$app_id = 'client_id';
-					break;
-			}
-           $link = $this->oauth_url . '?' . urldecode(http_build_query([
+            $app_id = 'app_id';
+            $redirect_uri = 'redirect_uri';
+            $additional_parameters = array();
+            switch ($this->network_name){
+                    case 'facebook':
+                            break;
+                    case 'linkedin':
+                            $app_id = 'client_id';
+                            $additional_parameters = array(
+                                'state' => $this->state,
+                            );
+                            break;
+                    case 'googleplus':
+                            $app_id = 'client_id';
+                            break;
+
+            }
+           $link = $this->oauth_url . '?' . urldecode(http_build_query(array(
                 $app_id         => $this->app_id,
-                'redirect_uri'  => $this->redirect_uri,
+                $redirect_uri   => $this->redirect_uri,
                 'response_type' => $this->response_type,    
                 'scope'         => $this->scope,
-				'state' 		=> $this->state,
-            ]));
-           //var_dump($link);
+            ) + $additional_parameters));
+
            header('Location:' . $link);
+           die();
     }
     
-    public function getFacebookLoginLink(){
-	
-        $link = $this->oauth_url . '?' . urldecode(http_build_query(array(
-                'app_id'        => $this->app_id,
-                'redirect_uri'  => $this->redirect_uri,
-                'response_type' => $this->response_type,    
-                'scope'         => $this->scope,
-            )));
-        return $link;
-    }
-    
-    public function getAccessToken($code = '', $state = ''){
-        
+    public function getAccessToken($code = '', $state = '') {
+
+	$post = false;
         $tokenInfo = array();
+        $header = '';
         
         if ($code != '') {
-			$url_params = array();
-			switch ($this->network_name){
-				case 'facebook':
-					$url_params = array(
-						'client_id'     => $this->app_id,
-						'redirect_uri'  => $this->redirect_uri,
-						'client_secret' => $this->app_secret_code,
-						'code'          => $code,
-					);
-					break;
-				case 'linkedin':
-					$url_params = array(
-						'code' => $code, 
-						'state' => $_GET['state'],
-						'grant_type' => 'authorization_code',
-						'redirect_uri' => $this->redirect_uri,
-						'client_id' => $this->app_id,
-						'client_secret' => $this->app_secret_code,
-						'format' => 'json',
-					);
-					break;
-			}
             
-             $url =  $this->access_token_url . '?' .urldecode(http_build_query($url_params));
+            $url_params = array();
             
+            switch ($this->network_name) {
+                    case 'facebook':
+                            $url_params = array(
+                                    'client_id'     => $this->app_id,
+                                    'redirect_uri'  => $this->redirect_uri,
+                                    'client_secret' => $this->app_secret_code,
+                                    'code'          => $code,
+                            );
+                            $post = false;
+                            $header = '';
+                            $url =  $this->access_token_url . '?' .urldecode(http_build_query($url_params));
+
+                            break;
+                    case 'linkedin':
+                            $url_params = array(
+                                    'code' => $code, 
+                                    'state' => $_GET['state'],
+                                    'grant_type' => 'authorization_code',
+                                    'redirect_uri' => $this->redirect_uri,
+                                    'client_id' => $this->app_id,
+                                    'client_secret' => $this->app_secret_code,
+                                    'format' => 'json',
+                            );
+                            $post = false;
+                            $header = '';
+                            $url =  $this->access_token_url . '?' .urldecode(http_build_query($url_params));
+
+                            break;
+                    
+                    case 'googleplus':
+                        //For google plus only
+                            $url_params = 
+                                    'code=' . urlencode($code) . '&' .  
+                                    'redirect_uri=' . $this->redirect_uri . '&' . 
+                                    'client_id=' . urlencode($this->app_id) . '&' . 
+                                    'client_secret=' . urlencode($this->app_secret_code) . '&' . 
+                                    //'format' => 'json',
+                                    'grant_type=' . 'authorization_code'
+                            ;
+                            $post = true;
+                            $header = array(
+                                        'Content-Type: application/x-www-form-urlencoded' ,
+                                        'Content-length:' . strlen($url_params)
+                                    );
+                            $url =  $this->access_token_url;
+                            break;
+                   
+
+            }
+            
+		
         } else {
              return false;
         }
-        
         $ch = curl_init();
         $options = array(
-                CURLOPT_URL 			=> $url,
-				CURLOPT_POST			=> true,
+                CURLOPT_URL 		=> $url,
+                CURLOPT_POST		=> $post,
                 CURLOPT_RETURNTRANSFER  => 1,
-                CURLOPT_TIMEOUT 		=> 10,
+                CURLOPT_TIMEOUT 	=> 10,
                 CURLOPT_SSL_VERIFYHOST  => 0,
-                CURLOPT_SSL_VERIFYPEER  => false
+                CURLOPT_SSL_VERIFYPEER  => false,
+                
         );
-        
+       
         curl_setopt_array($ch, $options);
-		
+        
+        if ($post) {
+            curl_setopt($ch, CURLOPT_HTTPHEADER, $header);
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $url_params);
+        }
+        
         $response = @curl_exec($ch);
-        
+
         $response_json = json_decode($response);
-        
+
         if (!isset($response_json->error->code)){ 
             if ($response){
 			
                 switch ($this->network_name){
-					case 'facebook':
-						parse_str($response, $tokenInfo);
-						break;
-					case 'linkedin':
-						return array('access_token' => $response_json->access_token);
-						break;
-				}
+                    case 'facebook':
+                        parse_str($response, $tokenInfo);
+                        break;
+                    case 'linkedin':
+                        return array('access_token' => $response_json->access_token);
+                        break;
+                }
 				
             } else {
                 return $response_json;
@@ -163,28 +203,93 @@ class SocialLogin //extends Model
     }
     
     public function getUserData($accessToken = '') {
-		$url_params = array();
-		switch ($this->network_name){
-			case 'facebook':
-				$url_params = array('access_token' => $accessToken);
-				break;
-			case 'linkedin':
-				$url_params = array();
-				break;
-		}
-		$options = array(
-			'http' => array(
-				'header'  => "Content-type: application/x-www-form-urlencoded\r\nAuthorization: Bearer " . $accessToken . "\r\nx-li-format: json",
-				'Connection' => 'Keep-Alive',
-				'host'    => $this->user_data_url,
-				'method'  => 'GET',
-				'content' => http_build_query($url_params),
-			),
-		);
-				
+        
+        $url_params = array();
+        $json = true;
+        
+        switch ($this->network_name){
+                case 'facebook':
+                        $url_params = array('access_token' => $accessToken);
+                        $json = true;
+                        break;
+                case 'linkedin':
+                        $url_params = array();
+                        $json = true;
+                        break;
+                case 'googleplus':
+                        $url_params = array();
+                        $json = true;
+                        break;
+
+        }
+        
+        $options = array(
+                'http' => array(
+                        'header'  => "Content-type: application/x-www-form-urlencoded\r\nAuthorization: Bearer " . $accessToken . "\r\nx-li-format: json",
+                        'Connection' => 'Keep-Alive',
+                        'host'    => $this->user_data_url,
+                        'method'  => 'GET',
+                        'content' => http_build_query($url_params),
+                ),
+        );
+		
         $url =  $this->user_data_url . '?' . urldecode(http_build_query($url_params));
-		$context  = stream_context_create($options);
-        $userInfo = json_decode(file_get_contents($url, false, $context), true);
+        $userRawInfo = file_get_contents($url, false, $context);
+        $context  = stream_context_create($options);
+        if ($json) {
+            $userRawInfo = json_decode(file_get_contents($url, false, $context), true);
+        } else {
+            $userRawInfo = file_get_contents($url, false, $context);
+        }
+        
+        $userInfo = [];
+        $userInfo['raw'] = $userRawInfo;
+        switch ($this->network_name){
+                case 'facebook':
+                        $userInfo['first_name'] = $userRawInfo['first_name'];
+                        $userInfo['last_name'] = $userRawInfo['last_name'];
+                        $userInfo['birthday'] = (string)strtotime($userRawInfo['birthday']);
+                        $userInfo['email'] = $userRawInfo['email'];
+                        $userInfo['headline'] = '';
+                        $userInfo['location'] = $userRawInfo['location']['name'];
+                        $userInfo['avatar'] = '';
+                        $userInfo['facebook_id'] = $userRawInfo['id'];
+                        $userInfo['linkedin_id'] = '';
+                        $userInfo['facebook_link'] = $userRawInfo['link'];
+                        $userInfo['linkedin_link'] = '';
+                        break;
+                case 'linkedin':
+                        $userInfo['first_name'] = $userRawInfo['firstName'];
+                        $userInfo['last_name'] = $userRawInfo['lastName'];
+                        $userInfo['birthday'] = '';
+                        $userInfo['email'] = '';
+                        $userInfo['headline'] = $userRawInfo['headline'];
+                        $userInfo['location'] = '';
+                        $userInfo['avatar'] = '';
+                        $userInfo['facebook_id'] = '';
+                        $userInfo['linkedin_id'] = $userRawInfo['id'];
+                        $userInfo['facebook_link'] = '';
+                        $userInfo['linkedin_link'] =  $userRawInfo['siteStandardProfileRequest']['url'];
+                        
+                        break;
+                    case 'googleplus':
+                        
+                        $userInfo['first_name'] = $userRawInfo['given_name'];
+                        $userInfo['last_name'] = $userRawInfo['family_name'];
+                        $userInfo['birthday'] = '';
+                        $userInfo['email'] = $userRawInfo['email'];
+                        $userInfo['headline'] = '';
+                        $userInfo['location'] = '';
+                        $userInfo['avatar'] = $userRawInfo['picture'];
+                        $userInfo['facebook_id'] = '';
+                        $userInfo['linkedin_id'] = '';
+                        $userInfo['google_id'] = $userRawInfo['id'];
+                        $userInfo['facebook_link'] = '';
+                        $userInfo['linkedin_link'] =  '';
+                        $userInfo['google_link'] = $userRawInfo['link'];
+
+                        break;
+        }
         
         return $userInfo;
     }
--- index.php
+++ index.php
@@ -1,37 +1,110 @@
 <?php
+	session_start();
+?>
+<!doctype html>
+<html>
+<head>
+	<meta charset='utf-8'>
+	<title>Social Login test</title>
+	<style>
+		html, body{
+			font-family:arial, sans-serif;
+		}
+		li {
+			list-style-type:none;
+			margin:4px auto ;
+		}
+	</style>
+</head>
+<body>
+<h3>
+	Login test
+</h3>
+<form method='post'>
+	<ul>
+            <li>
+                <input type='radio' name='login_method' value='facebook' id='facebook'><label for='facebook'>Facebook</label>
+            </li>
+            <li>
+                <input type='radio' name='login_method' value='linkedin' id='linkedin'><label for='linkedin'>LinkedIn</label>
+            </li>
+            <li>
+                <input type='radio' name='login_method' value='googleplus' id='googleplus'><label for='googleplus'>Google Plus</label>
+            </li>
+
+	</ul>
+	<div style='align:center;'>
+		<input type='submit' value='Login' name='login'>
+		<?php if (isset($_SESSION['login_method'])) { ?>
+			<input type='submit' value='Logout' name='logout'>
+		<?php } ?>
+	</div>
+</form>
+
+</body>
+</html>
+
+<?php
+
 error_reporting(E_ALL);
+
 include 'SocialLogin.php';
 
-$facebook = new SocialLogin('facebook');
-
-
-$facebook->app_id = '981066008590919';
-$facebook->app_secret_code = '6101529c823a704526074e2c937f9519';
-
-$facebook->redirect_uri = 'http://localhost/'; //The domain be setted in the Facebook App settings
-
-//For creating facebook login link e.g. for making <a href="http://facebook...">Login</a>,
-//use $facebook->getFacebookLoginLink();
-
-if (!isset($_GET['code'])) {
- 
-    $facebook->getAuthCode();
-} else {
-    
-    $facebook_response = $facebook->getAccessToken($_GET['code']);
-   
-    if (!isset($facebook_response->error->code)) {
-        
-        $user_data = $facebook->getUserData($facebook_response['access_token']);
-       
-        echo '<pre>';
-        var_dump($user_data);
-        echo '</pre>';
-    } else {
-        var_dump($facebook_response);
-    }
+if (isset($_POST['login_method'])) {
+	$_SESSION['login_method'] = $_POST['login_method'];
 }
- 
+if (isset($_POST['logout'])) {
+	session_destroy();
+	header('Location: ./');
+	die();
+}
+switch ($_SESSION['login_method']) {
+	case 'facebook':
+                $socialLogin = new SocialLogin('facebook');
+                $socialLogin->app_id = '';
+                $socialLogin->app_secret_code = '';
+                $socialLogin->redirect_uri = 'http://test.local/sociallogin'; //The domain be setted in the Facebook App settings
+                break;
+	case 'linkedin':
+                $socialLogin = new SocialLogin('linkedin');
+                $socialLogin->app_id = '';
+                $socialLogin->app_secret_code = '';
+                $socialLogin->redirect_uri = 'http://test.local/sociallogin'; 
+                break;
+	case 'googleplus':
+                $socialLogin = new SocialLogin('googleplus');
+                $socialLogin->app_id = '';
+                $socialLogin->app_secret_code = '';
+                $socialLogin->redirect_uri = 'http://localhost:8000/'; 
+                break;
+
+	default:
+		die();
+}
+
+	
+echo '<pre>';
+
+	if (!isset($_GET['code'])) {
+		$socialLogin->getAuthCode();
+		
+	} else if (isset($socialLogin)) {
+
+                
+		$socialLogin_response = $socialLogin->getAccessToken($_GET['code']);
+                
+		if (!isset($socialLogin_response->error->code)) {
+			if (is_array($socialLogin_response)) {
+                            $user_data = $socialLogin->getUserData($socialLogin_response['access_token']);
+                        } else {
+                            $user_data = $socialLogin->getUserData($socialLogin_response->access_token);
+                        }
+
+			var_dump($user_data);
+			
+		} else {
+		}
+	}
+echo '</pre>';
 
 ?>
-
